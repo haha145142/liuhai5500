13764cfd3f68c76b4ba3e4eb8656237f3592485c
haha145142 <276353662@qq.com>
2026-08-31 01:43:07 +0800

    feat: route fund quotes through self-calculated live valuation

--- src/lib/data/server.ts
+++ src/lib/data/server.ts
@@ -1,3 +1,4 @@
-export { getSnapshot, getNews, getFund, calcIndicators } from "./server-stable";
+export { getSnapshot, getNews, calcIndicators } from "./server-stable";
+export { getCalculatedFund as getFund } from "./live-valuation";
 export { getFundSectorQuotes } from "./fund-sector";
 export { searchFund, getFundRank, analyzeMarket, analyzeNews } from "./compat";
