461703422bb92eddf22fea501ebbdc36bbd36ae1
haha145142 <276353662@qq.com>
2026-08-31 17:05:54 +0800

    refactor: route news through quality pipeline

--- src/lib/data/server.ts
+++ src/lib/data/server.ts
@@ -1,4 +1,5 @@
-export { getSnapshot, getNews, calcIndicators } from "./server-stable";
+export { getSnapshot, calcIndicators } from "./server-stable";
+export { getNews } from "./server-news";
 export { getValidatedFund as getFund } from "./validated-fund";
 export { getFundSectorQuotes } from "./fund-sector";
 export { searchFund, getFundRank, analyzeMarket, analyzeNews } from "./compat";
