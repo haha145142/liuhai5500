22f4d5e553894f22e697487e7aafacff79e336e7
haha145142 <276353662@qq.com>
2026-08-31 13:20:12 +0800

    fix: export DeepSeek server functions

--- src/lib/data/server.ts
+++ src/lib/data/server.ts
@@ -2,3 +2,4 @@ export { getSnapshot, getNews, calcIndicators } from "./server-stable";
 export { getValidatedFund as getFund } from "./validated-fund";
 export { getFundSectorQuotes } from "./fund-sector";
 export { searchFund, getFundRank, analyzeMarket, analyzeNews } from "./compat";
+export { testDeepSeek, analyzeDeepSeek } from "./deepseek";
