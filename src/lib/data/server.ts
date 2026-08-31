e5bf2dbe409e9b0583465c35ddc60bb547d69abe
haha145142 <276353662@qq.com>
2026-08-31 21:40:55 +0800

    fix: route fund quotes through resilient fallback

--- src/lib/data/server.ts
+++ src/lib/data/server.ts
@@ -1,6 +1,6 @@
 export { getSnapshot, calcIndicators } from "./server-stable";
 export { getNews } from "./server-news";
-export { getValidatedFund as getFund } from "./validated-fund";
+export { getResilientFund as getFund } from "./resilient-fund";
 export { getFundSectorQuotes } from "./fund-sector";
 export { searchFund, getFundRank, analyzeMarket, analyzeNews } from "./compat";
 export { testDeepSeek, analyzeDeepSeek } from "./deepseek";
