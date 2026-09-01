6b40cd476e1a2a30ea27d65ca649bda8c319d179
haha145142 <276353662@qq.com>
2026-09-02 02:21:05 +0800

    feat: surface daily assessment on homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HomeDashboardV2 } from "@/components/home/HomeDashboardV2";
+import { TodayAssessment } from "@/components/home/TodayAssessment";
 
 export const Route = createFileRoute("/")({ component: Home });
-function Home() { return <HomeDashboardV2 />; }
+function Home() {
+  return (
+    <>
+      <TodayAssessment />
+      <HomeDashboardV2 />
+    </>
+  );
+}
