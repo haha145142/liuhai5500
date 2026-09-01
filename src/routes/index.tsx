8ee923967c9aadeddcfee6bf38bf83801cbbdd8b
haha145142 <276353662@qq.com>
2026-09-02 03:40:35 +0800

    refactor: make home dashboard single composition root

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HomeDashboardV2 } from "@/components/home/HomeDashboardV2";
-import { TodayAssessment } from "@/components/home/TodayAssessment";
 
 export const Route = createFileRoute("/")({ component: Home });
 function Home() {
-  return (
-    <>
-      <TodayAssessment />
-      <HomeDashboardV2 />
-    </>
-  );
+  return <HomeDashboardV2 />;
 }
